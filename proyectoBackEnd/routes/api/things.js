eef7bf5c60955adad5f09e6b12643cc9a5c71bcf
MOISES <kmsprado2546@gmail.com>
2019-07-31 00:52:02 -0600

    se agrega delete

--- proyectoBackEnd/routes/api/things.js
+++ proyectoBackEnd/routes/api/things.js
@@ -57,11 +57,10 @@ router.post('/new', function(req, res, next){
     });
 }); //Nuevo things
 
-router.put('/done/:thingID', function (req, res, next){
+router.put('/done/:thingId', function (req, res, next){
     var _thingId = req.params.thingId;
     var _thingUpds = req.body;
     var _thingUpdated = null;
-
     var newData = data.map(
         function(doc, i){
             if(doc._id == _thingId){
@@ -85,6 +84,26 @@ router.put('/done/:thingID', function (req, res, next){
         return res.status(200).json(_thingUpdated);
     });
 });// Set a thing as done
+
+router.delete('/delete/:thingId', function(req, res, next){
+    var _thingId = req.params.thingId;
+    var newData = data.filter(
+        function(doc, i){
+            if(doc._id == _thingId){
+                return false;
+            }
+            return true;
+        }
+    ); // end filter
+    data = newData;
+    fileModel.write(data, function(err){
+        if(err){
+            console.log(err);
+            return res.status(500).json({'Error':'Error al Eliminar la Data'});
+        }
+        return res.status(200).json({"delete": _thingId});
+    });
+}); //end delete
 
 fileModel.read(function(err, filedata){
     if(err){
