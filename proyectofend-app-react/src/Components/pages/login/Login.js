2ecd129dc0d104331cb0d44ab95c0e68dfc50deb
MOISES <kmsprado2546@gmail.com>
2019-08-04 13:23:03 -0600

    instalacion de Axios y conexxion entre BackEndy FrontEnd

--- proyectofend-app-react/src/Components/pages/login/Login.js
+++ proyectofend-app-react/src/Components/pages/login/Login.js
@@ -3,6 +3,7 @@ import Header from '../../generics/header/Header';
 import Body from '../../generics/body/Body';
 import Input from '../../generics/input/Input';
 
+import axios from 'axios';
 class Login extends Component{
     constructor(){
         super();
@@ -11,6 +12,7 @@ class Login extends Component{
             "txtEmail":""
         }
         this.onChangehandler = this.onChangehandler.bind(this);
+        this.onClickHandler = this.onClickHandler.bind(this);
     }
     render(){
         return (
@@ -35,6 +37,7 @@ class Login extends Component{
                      inputErrorMsg=""
                      inputChangeHandler={this.onChangehandler}
                    />
+                   <button onClick={this.onClickHandler}>Login</button>
                </Body>
             </div>
         );
@@ -42,6 +45,19 @@ class Login extends Component{
     onChangehandler(e){
        const {name, value} = e.currentTarget; //ES5 destructor de objetos || destructoring
        this.setState({...this.state, [name]:value});  
+    }
+    onClickHandler(e){
+        e.preventDefault();
+        e.stopPropagation();
+        //alert("Ohh hice click");
+        axios.post(
+            '/api/users/login', 
+            {...this.state}
+            ).then((resp)=>{
+                alert(resp)
+            }).catch((err) =>{
+                alert(err)
+            });
     }
 }
 
