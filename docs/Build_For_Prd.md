0d27fff0a84ddf29832e3b2cce0a057a159e224c
MOISES <kmsprado2546@gmail.com>
2019-08-05 01:16:42 -0600

    creacion del Build para produccion

---
+++ docs/Build_For_Prd.md
@@ -0,0 +1,13 @@
+# Como implementar en Producción
+
+## Requisitos
+1. Server (DigitalOcean, Vultr, AWS S, Heroku)
+2. HostName (Dominio)
+3. node.js
+4. git
+5. mongodb
+6. pm2 administrador de procesos de node.js
+
+## Build
+basckend, frontend
+Hosting (Shared hosting | Vps o Containers)
---
+++ proyectoBackEnd/public/precache-manifest.76a73bdb82539e65da191681f78185b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d1f37cecda6a35761a4666d193d887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1003bc0d6b78e9677833",
+    "url": "/static/css/main.2ae7b87a.chunk.css"
+  },
+  {
+    "revision": "c9882727b73083f21644",
+    "url": "/static/js/2.adbc70ac.chunk.js"
+  },
+  {
+    "revision": "1003bc0d6b78e9677833",
+    "url": "/static/js/main.938fcf58.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
