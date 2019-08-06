c9aa446e116131a1224d9ce66573f3d8a7ce8ff8
MOISES <kmsprado2546@gmail.com>
2019-08-06 17:23:38 -0600

    run BUILD

---
+++ proyectoBackEnd/public/precache-manifest.0958aa3c848b8211f679ef7ab11bfbe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce77535c48ada4f39fed1dd8b31687b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "589bbbbc7aa56094807e",
+    "url": "/static/css/main.ffe87b93.chunk.css"
+  },
+  {
+    "revision": "20f38d30d8de229ffd31",
+    "url": "/static/js/2.5e1d2999.chunk.js"
+  },
+  {
+    "revision": "589bbbbc7aa56094807e",
+    "url": "/static/js/main.90d4d8ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b47e80cee07d2ef215d28f6600274412",
+    "url": "/static/media/fondo.b47e80ce.jpg"
+  }
+]);
